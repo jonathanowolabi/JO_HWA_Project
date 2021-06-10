5a3a7a27619de57edc4d4f4d79cb27b0700c1210
jonathanowolabi <83285078+jonathanowolabi@users.noreply.github.com>
2021-06-10 11:27:33 +0100

    Initialize dev

---
+++ Back_end/HWA_Project_Backend/src/main/java/com/qa/HWAProject/HwaProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qa.HWAProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwaProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwaProjectBackendApplication.class, args);
+	}
+
+}
---
+++ Back_end/HWA_Project_Backend/src/test/java/com/qa/HWAProject/HwaProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.HWAProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwaProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
